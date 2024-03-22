b4d13ac156b7666f6ed100289b4b566e0aadcca7
ralenaNA <ralena@nailalliance.com>
2024-03-22 12:07:48 -0500

    Extend functionality to allow for different store views

--- Plugin/Block/Catnav.php
+++ Plugin/Block/Catnav.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Nailalliance\AddCategoryLink\Plugin\Block;
 
 use Magento\Framework\Data\Tree\NodeFactory;
@@ -31,20 +32,47 @@ class Catnav
 
     public function getNodeAsArray()
     {
+        $baseUrl = $this->urlInterface->getBaseUrl();
+
+        switch ($baseUrl) {
+            case "https://gelishmorgantaylor.co.uk/":
+                $name = "";
+                $id = "";
+                $url = "";
+                $path = "";
+                break;
+            case "https://entitybeauty.com/":
+                $name = "";
+                $id = "";
+                $url = "";
+                $path = "";
+                break;
+        };
+
         return [
-            'name' => __('Education'),
-            'id' => 'brand-educators-menu',
-            'url' => $this->urlInterface->getUrl('https://gelishmorgantaylor.education/'),
-            'target' => '_blank',
+            'name' => __($name),
+            'id' => $id,
+            'url' => $baseUrl . $path,
             'has_active' => false,
-            'is_active' => $this->isActive()
+            'is_active' => $this->isActive($baseUrl)
         ];
     }
-    
-    private function isActive()
+
+
+    private function isActive(string $storeUrl)
     {
-        $activeUrls = 'https://gelishmorgantaylor.education/';
+        $activeUrls = "";
         $currentUrl = $this->urlInterface->getCurrentUrl();
+
+        switch ($storeUrl) {
+            case "https://gelishmorgantaylor.co.uk/":
+                $activeUrls = 'https://gelishmorgantaylor.education/';
+                break;
+            case "https://entitybeauty.com/":
+                $activeUrls = 'https://entitybeauty.com/blog';
+                break;
+        }
+
         if (strpos($currentUrl, $activeUrls) !== false) {
             return true;
         }
