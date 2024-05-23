38632ea29076018c57d2743999c1b6c3baec1f72
ralenaNA <ralena@nailalliance.com>
2024-05-23 14:58:35 -0500

    Refactor for multiple links

--- Plugin/Block/Catnav.php
+++ Plugin/Block/Catnav.php
@@ -7,6 +7,10 @@ use Magento\Store\Model\StoreManagerInterface;
 
 class Catnav
 {
+    protected $nodeFactory;
+    protected $urlInterface;
+    protected $storeManager;
+
     public function __construct(
         NodeFactory $nodeFactory,
         \Magento\Framework\UrlInterface $urlInterface,
@@ -24,72 +28,78 @@ class Catnav
         $limit = 0
     ) {
         $storeCode = $this->storeManager->getStore()->getCode();
-        // $storeId = $this->storeManager->getStore()->getId(); // Alternatively, use store ID
         $allowedStores = ['entitybeautyview', 'gelishmorgantaylorview', 'default'];
 
         if (in_array($storeCode, $allowedStores)) {
-            $node = $this->nodeFactory->create(
-                [
-                    'data' => $this->getNodeAsArray(),
-                    'idField' => 'id',
-                    'tree' => $subject->getMenu()->getTree()
-                ]
-            );
-            $subject->getMenu()->addChild($node);
+            $nodes = $this->getNodesAsArray();
+            foreach ($nodes as $nodeData) {
+                $node = $this->nodeFactory->create(
+                    [
+                        'data' => $nodeData,
+                        'idField' => 'id',
+                        'tree' => $subject->getMenu()->getTree()
+                    ]
+                );
+                $subject->getMenu()->addChild($node);
+            }
         }
     }
 
-    public function getNodeAsArray()
+    public function getNodesAsArray()
     {
         $baseUrl = $this->urlInterface->getBaseUrl();
+        $nodes = [];
 
         switch ($baseUrl) {
             case "https://gelishmorgantaylor.co.uk/":
-                $name = "Education";
-                $id = "education-site-link";
-                $path = 'https://gelishmorgantaylor.education';
+                $nodes[] = [
+                    'name' => __('Education'),
+                    'id' => 'education-site-link',
+                    'url' => 'https://gelishmorgantaylor.education',
+                    'has_active' => false,
+                    'is_active' => $this->isActive('https://gelishmorgantaylor.education')
+                ];
                 break;
             case "https://entitybeauty.com/":
-                $name = "Blog";
-                $id = "entity-blog-link";
-                $path = 'https://entitybeauty.com/blog';
+                $nodes[] = [
+                    'name' => __('Blog'),
+                    'id' => 'entity-blog-link',
+                    'url' => 'https://entitybeauty.com/blog',
+                    'has_active' => false,
+                    'is_active' => $this->isActive('https://entitybeauty.com/blog')
+                ];
+                $nodes[] = [
+                    'name' => __('Warranty Registration'),
+                    'id' => 'warranty-link',
+                    'url' => 'https://entitybeauty.com/warrantyregistration',
+                    'has_active' => false,
+                    'is_active' => $this->isActive('https://entitybeauty.com/warrantyregistration')
+                ];
                 break;
             case "https://entitybeauty.co.uk/":
-                $name = "Blog";
-                $id = "entity-blog-link";
-                $path = 'https://entitybeauty.co.uk/blog';
+                $nodes[] = [
+                    'name' => __('Blog'),
+                    'id' => 'entity-blog-link',
+                    'url' => 'https://entitybeauty.co.uk/blog',
+                    'has_active' => false,
+                    'is_active' => $this->isActive('https://entitybeauty.co.uk/blog')
+                ];
+                $nodes[] = [
+                    'name' => __('Warranty Registration'),
+                    'id' => 'warranty-link',
+                    'url' => 'https://entitybeauty.co.uk/warrantyregistration',
+                    'has_active' => false,
+                    'is_active' => $this->isActive('https://entitybeauty.co.uk/warrantyregistration')
+                ];
                 break;
         };
 
-        return [
-            'name' => __($name),
-            'id' => $id,
-            'url' => $path,
-            'has_active' => false,
-            'is_active' => $this->isActive($baseUrl)
-        ];
+        return $nodes;
     }
 
-
-    private function isActive(string $storeUrl)
+    private function isActive(string $nodeUrl)
     {
-        $activeUrls = "";
         $currentUrl = $this->urlInterface->getCurrentUrl();
-
-        switch ($storeUrl) {
-            case "https://gelishmorgantaylor.co.uk/":
-                $activeUrls = 'https://gelishmorgantaylor.education/';
-                break;
-            case "https://entitybeauty.com/":
-                $activeUrls = 'https://entitybeauty.com/blog';
-                break;
-            default:
-                break;
-        }
-
-        if (strpos($currentUrl, $activeUrls) !== false) {
-            return true;
-        }
-        return false;
+        return strpos($currentUrl, $nodeUrl) !== false;
     }
 }
